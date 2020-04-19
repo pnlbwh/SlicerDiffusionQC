542827fa691a567f00f14fab1b44e9fe11cbd104
Billah, Tashrif <tbillah@bwh.harvard.edu>
2020-04-19 12:04:41 -0400

    update version tag

--- diffusionqclib/setup.py
+++ diffusionqclib/setup.py
@@ -9,7 +9,7 @@ with open(path.join(here, 'README.md'), encoding='utf-8') as f:
 
 setup(
     name='diffusionqclib',  # Required
-    version='2.0.4',  # Required
+    version='2.1',  # Required
     description='Library to support SlicerDiffusionQC module',  # Optional
     long_description=long_description,  # Optional
     long_description_content_type='text/markdown',  # Optional (see note above)
